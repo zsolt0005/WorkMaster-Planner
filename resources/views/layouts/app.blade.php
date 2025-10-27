e17dcde9a241d66efc8596df6e50df4f39578ab9
Zsolt Dome <dome@efabrica.sk>
2025-10-27 20:57:42 +0100

    WP-2 | removed unnecessary comments, removed footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,42 +1,35 @@
 <!doctype html>
 <html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>@yield('title', 'WorkMaster Planner')</title>
+        <title>@yield('title', 'WorkMaster Planner')</title>
 
-    <!-- FONTS -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet">
+        <!-- FONTS -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet">
 
-    <!-- CSS FILES -->
-    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/bootstrap-icons.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/templatemo-leadership-event.css') }}" rel="stylesheet">
-</head>
+        <!-- CSS FILES -->
+        <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/bootstrap-icons.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/templatemo-leadership-event.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        {{-- HEADER --}}
+        @include('parts.header')
 
-<body>
+        {{-- CONTENT --}}
+        <main>
+            @yield('content')
+        </main>
 
-{{-- HEADER --}}
-@include('parts.header')
-
-<main>
-    @yield('content')
-</main>
-
-{{-- FOOTER --}}
-@if (!request()->routeIs('index'))
-    @include('parts.footer')
-@endif
-
-<!-- JAVASCRIPT FILES -->
-<script src="{{ asset('js/jquery.min.js') }}"></script>
-<script src="{{ asset('js/bootstrap.min.js') }}"></script>
-<script src="{{ asset('js/jquery.sticky.js') }}"></script>
-<script src="{{ asset('js/click-scroll.js') }}"></script>
-<script src="{{ asset('js/custom.js') }}"></script>
-
-</body>
+        <!-- JAVASCRIPT FILES -->
+        <script src="{{ asset('js/jquery.min.js') }}"></script>
+        <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+        <script src="{{ asset('js/jquery.sticky.js') }}"></script>
+        <script src="{{ asset('js/click-scroll.js') }}"></script>
+        <script src="{{ asset('js/custom.js') }}"></script>
+    </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-// index
 Route::get('/', static function () {
     return view('index');
 })->name('index');
 
-// dashboard
 Route::get('/dashboard', static function () {
     return view('dashboard');
 })->name('dashboard');
 
-// people_management
 Route::get('/people_management', static function () {
     return view('people_management');
 })->name('people_management');
 
-// profile
 Route::get('/profile', static function () {
     return view('profile');
 })->name('profile');
