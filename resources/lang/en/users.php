d984355b0fc93735b26ea7a21727523172330e0a
gregorikmichal-wq <gregorikmichal@gmail.com>
2025-12-08 20:41:55 +0100

    gregorik/create_users (#22)
    Co-authored-by: Zsolt Dome <dome@efabrica.sk>

--- resources/lang/en/users.php
+++ resources/lang/en/users.php
@@ -4,9 +4,16 @@ return [
     'headers' => [
         'people-management' => 'People Management',
         'users' => 'Users',
+        'create-user' => 'Create user',
     ],
     'forms' => [
         'assign-roles' => 'Assign/unassign roles',
         'currently-assigned' => 'Currently assigned',
+        'name' => 'Name',
+        'full-name' => 'Full Name',
+        'email' => 'Email',
+        'password' => 'Password',
+        'password-confirmation' => 'Password Confirmation',
+        'new-password' => 'New Password',
     ],
 ];
