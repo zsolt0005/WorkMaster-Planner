b3cc816727d9e9c6c50de4e7c0a5144982c0d9b4
gameblaer <vrtak.martin@gmail.com>
2025-12-13 08:25:15 +0100

    fixed string instead int in month

--- app/Http/Controllers/GenerateAttendanceController.php
+++ app/Http/Controllers/GenerateAttendanceController.php
@@ -59,14 +59,14 @@ final class GenerateAttendanceController extends AController
 
         $startHour = 6;
         $year = (int)date('Y');
-        $month = 1;
+        $month = "1";
 
         $daysCount = 7;
         if ($weekOrMonth !== 'week') {
             $periodDate = new DateTimeImmutable("$year-$currentPeriod-01");
             $daysCount = (int)$periodDate->format('t');
         } else {
-            $month = new DateTimeImmutable()->setISODate($year, $currentPeriod);
+            $month = (string)new DateTimeImmutable()->setISODate($year, $currentPeriod);
         }
 
         for ($day = 1; $day <= $daysCount; $day++) {
